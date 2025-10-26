feca705bd08bf84d3149b6a34d4068024db5dbd4
Christian Renggli <49160827+Zauberhut@users.noreply.github.com>
2025-10-26 10:25:41 +0100

    Refactor rotary encoder functions and event handling

--- rotary.ts
+++ rotary.ts
@@ -6,37 +6,37 @@ const enum JoyPiAdvancedDirection {
     //% block.loc.de="links"
     counterclockwise = 4
 }
-//% weight=110  color=#3a4cf7
+
+//% weight=110 color=#3a4cf7
 namespace informatiktheater {
     const rotaryDTPin = DigitalPin.P2;
     const rotaryCLKPin = DigitalPin.P1;
     const rotarySWPin = DigitalPin.P14;
     const KYEventID = 3100;
+
     let directionIndicator = 1;
     let currentCLK = 0;
     let currentDT = 0;
     let lastCLK = 0;
     let EvCounter = 1;
-    let lastPressed = 1;
-    let pressedID = 5600;
 
     // Function to decide the direction in which the Encoder is being turned
-   function RotaryEncoder() {
-    if (currentCLK != lastCLK) {
-        directionIndicator = (currentDT != currentCLK) ? 1 : 0;
+    function RotaryEncoder() {
+        if (currentCLK != lastCLK) {
+            // Determine direction based on currentDT and currentCLK
+            directionIndicator = (currentDT != currentCLK) ? 1 : 0;
 
-        EvCounter += 1;
-        if (EvCounter % 2 == 0) { // Reduce event flooding
-            const eventDirection = directionIndicator == 1 
-                ? JoyPiAdvancedDirection.clockwise 
-                : JoyPiAdvancedDirection.counterclockwise;
-                
-            control.raiseEvent(KYEventID + eventDirection, eventDirection);
+            EvCounter += 1;
+            if (EvCounter % 2 == 0) { // Reduce event flooding
+                const eventDirection = directionIndicator == 1 
+                    ? JoyPiAdvancedDirection.clockwise 
+                    : JoyPiAdvancedDirection.counterclockwise;
+
+                control.raiseEvent(KYEventID + eventDirection, eventDirection);
+            }
+            lastCLK = currentCLK;
         }
-        lastCLK = currentCLK;
     }
-}
-
 
     /** 
       * Initializes the rotary encoder
@@ -46,46 +46,43 @@ namespace informatiktheater {
     //% subcategory="Drehknopf"
     //% weight=150
     export function initializeRotaryEncoder() {
-        led.enable(false)
+        led.enable(false);
 
         pins.setPull(rotaryDTPin, PinPullMode.PullUp);
         pins.setPull(rotarySWPin, PinPullMode.PullUp);
-        // Interrupt the code on a rising edge on the rotaryCLKPin to execute the RotaryEncoder() function
+
+        // Interrupt on rising edge of rotaryCLKPin
         pins.onPulsed(rotaryCLKPin, PulseValue.High, function () {
-            currentCLK = 1
-            RotaryEncoder()
+            currentCLK = 1;
+            RotaryEncoder();
+        });
 
-        })
-
-        // Interrupt the code on a falling edge on the rotaryCLKPin to execute the RotaryEncoder() function
+        // Interrupt on falling edge of rotaryCLKPin
         pins.onPulsed(rotaryCLKPin, PulseValue.Low, function () {
-            currentCLK = 0
-            RotaryEncoder()
+            currentCLK = 0;
+            RotaryEncoder();
+        });
 
-        })
-
+        // Interrupt on rising edge of rotaryDTPin
         pins.onPulsed(rotaryDTPin, PulseValue.High, function () {
-            currentDT = 1
+            currentDT = 1;
+        });
 
-        })
-
-        // Interrupt the code on a falling edge on the rotaryCLKPin to execute the RotaryEncoder() function
+        // Interrupt on falling edge of rotaryDTPin
         pins.onPulsed(rotaryDTPin, PulseValue.Low, function () {
-            currentDT = 0
-
-        })
-
+            currentDT = 0;
+        });
     }
 
-     /**
+    /**
       * Event that is executed as soon as the rotary encoder is turned in the corresponding direction
-      * @param JoyPiAdvancedDirection  Direction to be listened to
+      * @param JoyPiAdvancedDirection Direction to be listened to
       */
     //% block="on rotary encoder turned in direction %direction"
     //% block.loc.de="Wenn der Drehknopf nach %direction gedreht wird"
     //% subcategory="Drehknopf"
     //% weight=100
-    export function rotaryEncoderonTurned(direction: JoyPiAdvancedDirection , handler: () => void) {
+    export function onRotaryEncoderTurned(direction: JoyPiAdvancedDirection, handler: () => void) {
         control.onEvent(KYEventID + direction, direction, handler);
     }
 
@@ -98,8 +95,7 @@ namespace informatiktheater {
     //% weight=90
     export function rotaryEncoderonPressEvent(handler: () => void) {
         pins.onPulsed(rotarySWPin, PulseValue.Low, function () {
-            handler()
-        })
+            handler();
+        });
     }
-
 }
