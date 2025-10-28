7aa392148f7689524b6b7076753ada2f47025f97
Christian Renggli <49160827+Zauberhut@users.noreply.github.com>
2025-10-28 14:20:26 +0100

    Update joystick.ts

--- joystick.ts
+++ joystick.ts
@@ -1,40 +1,116 @@
-namespace JoyPiAdvanced {
-    const joystickButtonPin = DigitalPin.P16
-  
+
+    export class GroveJoystick
+    {
+        /**
+         * Detect position from Grove - Thumb Joystick
+         * @param xPin
+         * @param yPin
+         */
+
+        enum GroveJoystickKey {
+    //% block="None"
+    None = 0,
+    //% block="Right"
+    Right = 1,
+    //% block="Left"
+    Left = 2,
+    //% block="Up"
+    Up = 3,
+    //% block="Down"
+    Down = 4,
+    //% block="Upper left"
+    UL = 5,
+    //% block="Upper right"
+    UR = 6,
+    //% block="Lower left"
+    LL = 7,
+    //% block="Lower right"
+    LR = 8,
+    //% block="press"
+    Press = 9
+}
+/**
+ * Functions to operate Grove module.
+ */
+//% weight=10 color=#9F79EE icon="\uf1b3" block="Grove"
+//% groups='["Thumbjoystick"]'
+namespace grove {
     /**
-     * Returns the current X-value of the joystick
+     * 
      */
-    //% block="joystick X value"
-    //% subcategory="Joystick"
-    //% weight=100
-    export function joystickGetXValue(): number {
-      return adcReadValue(2)
+        joyread(xPin: AnalogPin, yPin: AnalogPin): number {
+
+            let xdata = 0, ydata = 0, result = 0;
+            if (xPin && yPin) {
+                xdata = pins.analogReadPin(xPin);
+                ydata = pins.analogReadPin(yPin);
+                if (xdata > 1000) {
+                    result = GroveJoystickKey.Press;
+                }
+                else if (xdata > 600) {
+                    if (ydata > 600) result = GroveJoystickKey.UR;
+                    else if (ydata < 400) result = GroveJoystickKey.LR;
+                    else result = GroveJoystickKey.Right;
+                }
+                else if (xdata < 400) {
+                    if (ydata > 600) result = GroveJoystickKey.UL;
+                    else if (ydata < 400) result = GroveJoystickKey.LL;
+                    else result = GroveJoystickKey.Left;
+                }
+                else {
+                    if (ydata > 600) result = GroveJoystickKey.Up;
+                    else if (ydata < 400) result = GroveJoystickKey.Down;
+                    else result = GroveJoystickKey.None;
+                }
+            }
+            else {
+                result =  GroveJoystickKey.None;
+            }
+            return result;
+        }
     }
-  
+/**
+     * get Joystick key
+     * 
+     */
+    //% blockId=grove_getjoystick block="get joystick key at|%xpin|and|%ypin"
+    //% group="Thumbjoystick" xpin.defl=AnalogPin.P0 ypin.defl=AnalogPin.P1
+    export function getJoystick(xpin: AnalogPin, ypin: AnalogPin): number {
+        return joystick.joyread(xpin, ypin);
+    }
+  /**
+     * Converts the key name to a number
+     * Useful for comparisons
+     */
+    //% blockId=joystickkey block="%key"
+    //% group="Thumbjoystick"
+    export function joystickkey(key: GroveJoystickKey): number {
+        return key;
+    }
+
     /**
-     * Returns the current Y-value of the joystick
+     * Do something when a key is detected by Grove - Thumb Joystick
+     * @param key type of joystick to detect
+     * @param xpin
+     * @param ypin
+     * @param handler code to run
      */
-    //% block="joystick Y value"
-    //% subcategory="Joystick"
-    //% weight=90
-    export function joystickGetYValue(): number {
-      return adcReadValue(1)
+    //% blockId=grove_joystick_create_event block="on Key|%key at |%xpin|and|%ypin"
+    //% group="Thumbjoystick" xpin.defl=AnalogPin.P0 ypin.defl=AnalogPin.P1
+
+    export function onJoystick(key: GroveJoystickKey, xpin: AnalogPin, ypin: AnalogPin, handler: () => void) {
+        control.onEvent(joystickEventID, key, handler);
+        control.inBackground(() => {
+            while(true) {
+                const key = joystick.joyread(xpin, ypin);
+                if (key != lastJoystick) {
+                    lastJoystick = key; 
+                    control.raiseEvent(joystickEventID, lastJoystick);
+                }
+                basic.pause(50);
+            }
+        })
+        
     }
-  
-    /**
-     * Checks the current button state of the joystick. True means that the button is pressed. False means that the button is not pressed.
-     */
-    //% block="joystick button state"
-    //% subcategory="Joystick"
-    //% weight=80
-    export function joystickCheckButton(): boolean {
-      led.enable(false)
-      if(!pins.digitalReadPin(joystickButtonPin)){
-        return true
-      }
-      else {
-        return false
-      }
-    }
-  }
-  
+}
+    
