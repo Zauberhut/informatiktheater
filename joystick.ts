b21ed06374407bbc599ce428ff47111e3bfd342a
Christian Renggli <49160827+Zauberhut@users.noreply.github.com>
2025-10-31 21:13:34 +0100

    Update joystick.ts

--- joystick.ts
+++ joystick.ts
@@ -8,7 +8,7 @@ namespace JoyPiAdvanced {
     //% subcategory="Joystick"
     //% weight=100
     export function joystickGetXValue(): number {
-      return adcReadValue(2);
+      return analogReadPin(2);
     }
   
     /**
@@ -18,7 +18,7 @@ namespace JoyPiAdvanced {
     //% subcategory="Joystick"
     //% weight=90
     export function joystickGetYValue(): number {
-      return adcReadValue(1);
+      return analogReadPin(1);
     }
   
     /**
