7aceb61a0239e8e2e2f3b5ea6abf4f4c1add2096
Christian Renggli <49160827+Zauberhut@users.noreply.github.com>
2025-10-31 21:46:09 +0100

    Update joystick.ts

--- joystick.ts
+++ joystick.ts
@@ -1,4 +1,17 @@
 namespace informatiktheater {
+
+   /**
+    * Measures the current state of the potentiometer as a voltage. A higher voltage means that the potentiometer is positioned further to the right
+    */
+    //% block="potentiometer voltage"
+    //% subcategory="Potentiometer"
+    //% weight=100
+    export function potentiometerVoltage(): number {
+      return adcReadVoltage(3)
+    }
+  
+
+    
     const joystickButtonPin = DigitalPin.P16;
 
     /**
